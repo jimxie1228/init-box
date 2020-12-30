2f58f554a9bed6061a8f07916439e35d605fee65
Jim Xie <jimxie@umich.edu>
2020-12-30 11:45:20 -0500

    format better

--- init-box.sh
+++ init-box.sh
@@ -4,21 +4,23 @@
 # --------------Git Access--------------
 # Description: configure ssh access to jimxie@umich.edu
 # Dependencies: ssh-keygen
-#               ed22519 algorithm https://docs.github.com/en/free-pro-team@latest/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent
+#               ssh-agent
 #               
 echo '================Configuring Git Aceess================'
 if [ ! -d "~/.ssh" ] 
 then
     mkdir ~/.ssh
 fi
-echo 'Please put the ssh key under ~/.ssh/jimxie_ssh_id'
 ssh-keygen -t rsa -b 4096 -C "jimxie@umich.edu" -f ~/.ssh/jimxie_ssh_id
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/jimxie_ssh_id
 
 # add ssh-key to github account
+echo
+echo 'Downloading xclip'
 sudo apt-get install xclip
 xclip -selection clipboard < ~/.ssh/jimxie_ssh_id.pub
+echo
 echo 'SSH key copied onto the clipboard'
 echo 'Add ssh public key to github account following steps:'
 echo '1. Go to github account -> Settings'
@@ -34,4 +36,5 @@ echo
 while [[ ! $REPLY =~ ^[Yy]$ ]]
 do
     read -p "Please add the ssh key. Done? (y/n)" -n 1 -r
+    echo
 done
